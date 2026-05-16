8a905ad99bce55e77d74e1c72ec9fff7f3bceaa3
RajaMohammed07 <rajamohammed.m2021rvs@gmail.com>
2026-05-16 11:47:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "PASTE_YOUR_NEW_API_KEY_HERE";
+const API_KEY = "AIzaSyAQv0YJ9PR5WFS2KvXLvzElQXzLFoLxDUk";
 
 const API_URL =
     "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
